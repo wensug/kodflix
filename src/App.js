09550f30b329a6b04d515d47191d743844d56713
Wendy Gonzalez <wensug@gmail.com>
2019-03-22 19:09:34 +0000

    Update the title to welcome users123

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-             <h1>Welcome to Kodflix!!!</h1>
+             <h1>Welcome to Kodflix 123!!!</h1>
           </p>
           <a
             className="App-link"
