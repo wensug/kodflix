e335d2ac27cec1f007491b295b580e4690bb547c
Wendy Gonzalez <wensug@gmail.com>
2019-04-01 15:56:41 +0100

    Add Movie Cover & Replace TV show with covers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import cover_1 from './images/captainamerica.jpg'
+import cover_2 from './images/sing.jpg'
+import cover_3 from './images/drstrange.jpg'
+import cover_4 from './images/exmachina.jpg'
+import cover_5 from './images/logan.jpg'
+import cover_6 from './images/passengers.jpg'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-             <h1>Welcome to Kodflix 123</h1>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className='container'>
+          <div className='item'>
+            <img src={cover_1} alt='Captain America' />
+          </div>
+          <div className='item'>
+            <img src={cover_2} alt='Sing' />
+          </div>
+
+          <div className='item'>
+            <img src={cover_3} alt='Dr Strange' />
+          </div>
+        </div>
+        <div className='container'>
+          <div className='item'>
+            <img src={cover_4} alt='Exmachina' />
+          </div>
+          <div className='item'>
+            <img src={cover_5} alt='Logan' />
+          </div>
+          <div className='item'>
+            <img src={cover_6} alt='Passengers' />
+          </div>
+        </div>
       </div>
     );
   }
