86a3f3273bb1558bf5121db118e1f9d8838af425
Wendy Gonzalez <wensug@gmail.com>
2019-04-08 22:23:46 +0100

    Improve the gallery layout #11

--- src/gallery/Gallery.js
+++ src/gallery/Gallery.js
@@ -11,20 +11,16 @@ import cover_6 from './images/passengers.jpg'
 export default class Gallery extends React.Component {
     render() {
         return (
-            <div>
-                <div className="container">
-                    <Tvshow id='captain-america' name='Captain America' cover={cover_1} />
-                    <Tvshow id='sing' name='Sing'  cover={cover_2} />
-                    <Tvshow id='dr-strange' name='Dr Strange'  cover={cover_3} />
-                </div>
-                <div className='container'>
-                    <Tvshow id='ex-machina' name='Exmachina' cover={cover_4} />
-                    <Tvshow id='logan' name='Logan'  cover={cover_5} />
-                    <Tvshow id='passengers' name='Passengers' cover={cover_6} />
-                </div>
-           </div>
+            <div className="container">
+                <Tvshow id='captain-america' name='Captain America' cover={cover_1} />
+                <Tvshow id='sing' name='Sing' cover={cover_2} />
+                <Tvshow id='dr-strange' name='Dr Strange' cover={cover_3} />
+                <Tvshow id='ex-machina' name='Exmachina' cover={cover_4} />
+                <Tvshow id='logan' name='Logan' cover={cover_5} />
+                <Tvshow id='passengers' name='Passengers' cover={cover_6} />
+            </div>
         );
     }
- }
+}
 
 
