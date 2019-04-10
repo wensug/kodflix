dcc5d698795bf53ec54f8974fb7202c169570a9a
Wendy Gonzalez <wensug@gmail.com>
2019-04-10 15:19:30 +0100

    Introduce component state #13 Remove commented code

--- src/Details.js
+++ src/Details.js
@@ -24,25 +24,3 @@ export default class Details extends React.Component {
 
     }
 }
-// export default function Details() {
-//     constructor() {
-//         super();
-
-//         this.state = {
-//             message: 'Hello, this will be the details page for each Movie & TV Show :)'
-//         };
-//     }
-
-//     componentDidMount() {
-//         setTimeout(() => {
-//             this.setState({ message: 'Coming soon! :)' });
-//         }, 3000);
-//     }
-//     return (
-//         <div>
-//             <h1>{this.state.message}</h1>
-//             <Link to='/'>Back to the Home Page</Link>
-//         </div>
-//     )
-
-// }
