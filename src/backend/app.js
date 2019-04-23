cb519cfb0f20e27ead84539e7663ed7d190825c7
Wendy Gonzalez <wensug@gmail.com>
2019-04-23 14:57:29 +0100

    Return a list of shows from the backend #19

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
+const getShows = require('./shows');
 
-app.get('/', (req, res) => res.send('Hello World!!!'));
+app.get('/rest/shows', (req, res) => res.send(getShows()));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/backend/shows.js
@@ -0,0 +1,33 @@
+module.exports = function getShows() {
+    return [ {
+        id: 'captain-america',
+        name: 'Captain America',
+        synopsis: `Steve Rogers, a rejected military soldier transforms into Captain America after taking a dose of a "Super-Soldier serum". 
+            But being Captain America comes at a price as he attempts to take down a war monger and a terrorist organization.`
+    }, {
+        id: 'sing',
+        name: 'Sing',
+        synopsis: `In a city of humanoid animals, a hustling theatre impresario's attempt to save his theatre with a singing competition 
+            becomes grander than he anticipates even as its finalists find that their lives will never be the same.`
+    }, {
+        id: 'dr-strange',
+        name: 'Dr Strange',
+        synopsis: `After a horrible car accident, brilliant neurosurgeon Stephen Strange must find a way and a cure to healing his hands. 
+            His quest brings him to Kamar-Taj in Nepal where he studies under the Ancient One, who teaches him the ways of magic and alternate realities`
+    }, {
+        id: 'ex-machina',
+        name: 'Exmachina',
+        synopsis: `Caleb, an employee at a Google-like company, is selected to visit the home of the CEO, Nathan. But while there, 
+            Nathan shows Caleb what he's been working on: Ava. She, the greatest achievement in the history of the human race.`
+    }, {
+        id: 'logan',
+        name: 'Logan',
+        synopsis: `In the near future, a weary Logan cares for an ailing Professor X at a remote outpost on the Mexican border. 
+            His plan to hide from the outside world gets upended when he meets a young mutant who is very much like him. `
+    }, {
+        id: 'passengers',
+        name:'Passengers',
+        synopsis: `A spacecraft traveling to a distant colony planet and transporting thousands of people has a malfunction 
+            in its sleep chambers. As a result, two passengers are awakened 90 years early.`
+    }];
+}
